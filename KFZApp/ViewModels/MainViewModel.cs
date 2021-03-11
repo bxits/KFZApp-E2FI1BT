ded4907c108d3608f4f9a654a2b0247fcdd5440e
Roodygar <34479694+Roodygar@users.noreply.github.com>
2021-03-11 09:11:50 +0100

    DataBinding an die AlleKFZs-Liste. XAML: xNameSpace, DataContext, Binding ViewModel: Property AlleKFZs angelegt.

--- KFZApp/ViewModels/MainViewModel.cs
+++ KFZApp/ViewModels/MainViewModel.cs
@@ -12,12 +12,15 @@ namespace KFZApp.ViewModels
     {
         public List<KFZ> AlleKFZs { get; set; }
 
-        public MainViewModel()
+        
+        public MainViewModel() //Standardkonstruktor
         {
             AlleKFZs = new List<KFZ>();
-            AlleKFZs.Add(new KFZ() { Kennzeichen = "S-RT 584" });
-            AlleKFZs.Add(new KFZ() { Kennzeichen = "RT-XD 5213" });
-            AlleKFZs.Add(new KFZ() { Kennzeichen = "B-BD 4302" });
+            AlleKFZs.Add(new KFZ() { Kennzeichen = "S-RT 584", Typ="SUV" });
+            AlleKFZs.Add(new KFZ() { Kennzeichen = "RT-XD 5213", Typ="Cabrio" });
+            AlleKFZs.Add(new KFZ() { Kennzeichen = "B-BD 4302", Typ="Crossover" });
+
+           
         }
 
         public event PropertyChangedEventHandler PropertyChanged;

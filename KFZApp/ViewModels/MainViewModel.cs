4fbcbc55fc28b7e6575767db3e79914acc5da573
Roodygar <34479694+Roodygar@users.noreply.github.com>
2021-03-11 08:15:41 +0100

    Projektdateien hinzufügen.

---
+++ KFZApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KFZApp.Model;
+using System.Threading.Tasks;
+
+namespace KFZApp.ViewModels
+{
+    class MainViewModel
+    {
+        public List<KFZ> AlleKFZs { get; set; }
+
+        public MainViewModel()
+        {
+            AlleKFZs = new List<KFZ>();
+            AlleKFZs.Add(new KFZ() { Kennzeichen = "S-RT 584" });
+            AlleKFZs.Add(new KFZ() { Kennzeichen = "RT-XD 5213" });
+            AlleKFZs.Add(new KFZ() { Kennzeichen = "B-BD 4302" });
+        }
+    }
+}
